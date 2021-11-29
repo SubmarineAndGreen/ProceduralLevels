f83dc276f7a5651c8fb196ea978022e09d9fe48c
SubmarineAndGreen <scythed66@gmail.com>
2021-11-29 20:58:10 +0100

    enemy test 1

--- Assets/_Scripts/Enemies/TestEnemy.cs
+++ Assets/_Scripts/Enemies/TestEnemy.cs
@@ -3,27 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEnemy : MonoBehaviour {
+    public float gravity = 9.8f;
     public float springMaxForceDistance = 0f;
     public float springMinForceDistance = 0.5f;
     public float maxSpringForce = 1f;
     public float minSpringForce = 0f;
-    private Ray springRay;
     private RaycastHit springRayHit;
-    private Rigidbody rb;
     private int springRayMask;
+    private Rigidbody rb;
+    public float pushAwayForce = 0.3f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
-
-        springRay = new Ray(transform.position, Vector3.down);
         springRayHit = new RaycastHit();
-    }
-
-    private void Start() {
-
-        // for(float test =  1; test < 2; test += 0.1f) {
-        //    Debug.Log(test + "remapped: " + MathUtils.remap(test, springMaxForceDistance, springMinForceDistance, 0, 1)); 
-        // }
     }
 
     void Update() {
@@ -31,12 +23,25 @@ public class TestEnemy : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if(Physics.Raycast(springRay, out springRayHit, springMaxForceDistance)) {
+        if(Physics.Raycast(transform.position, Vector3.down, out springRayHit, springMinForceDistance)) {
+
             float t = MathUtils.remap(springRayHit.distance, springMaxForceDistance, springMinForceDistance, 0, 1);
-            float springForce = Mathf.Lerp(maxSpringForce, minSpringForce, t);
+            float springForce = Mathf.Lerp(minSpringForce, maxSpringForce, 1 - t);
              
-            Debug.Log(springForce);
             rb.AddForce(Vector3.up * springForce, ForceMode.Force);
+        }
+        rb.AddForce(Vector3.down * gravity, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        pushAwayFromCollision(other.contacts);
+    }
+
+    private void pushAwayFromCollision(ContactPoint[] contactPoints) {
+        Debug.Log("push");
+        foreach(ContactPoint contact in contactPoints) {
+            Vector3 collisionDirection = transform.position - contact.point;
+            rb.AddForce(collisionDirection.normalized * pushAwayForce, ForceMode.Impulse);
         }
     }
 }
